f6bb91b36cedd939c5784f04f7c8513e096790d5
Benefrancis <benefrancis@gmail.com>
2023-03-16 20:53:26 -0300

    Adcionando comentários

---
+++ src/Demonstrando.java
@@ -0,0 +1,24 @@
+/**
+ * Demonstrando que caractere pode armazenar números decimais
+ *
+ * @author <a href="https://github.com/Benefrancis">Benefrancis do Nascimento</a>
+ */
+public class Demonstrando {
+    public static void main(String[] args) {
+
+        //Atribuindo um número inteiro em um caractere
+        char caractere = 0;
+
+        //Realizando comparação numérica com um caractere
+        while (caractere < 6556) {
+            //Pegando o resto da divisão de um valor caractere (dividendo)
+            //Por um divisor (35)
+            if (caractere % 99 == 0 )
+                System.out.println(caractere + "\t ");
+            else
+                System.out.print(caractere + "\t ");
+            //Somando +1 ao valor atual da variável caractere
+            caractere++;
+        }
+    }
+}
