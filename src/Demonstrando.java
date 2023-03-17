aa7f1b7878f0a21a2a702d5a7be8a9a956cbe416
Benefrancis <benefrancis@gmail.com>
2023-03-17 00:22:45 -0300

    Pequena correção lógica para imprimir o número ao lado do caracter correspondente

--- src/Demonstrando.java
+++ src/Demonstrando.java
@@ -6,19 +6,23 @@
 public class Demonstrando {
     public static void main(String[] args) {
 
+        int numero = 1;
         //Atribuindo um número inteiro num tipo caractere
-        char caractere = 0;
+        char caractere = 1;
 
         //Realizando comparação numérica com um tipo caractere
-        while (caractere < 6556) {
+        while (caractere <= 65534) {
             //Pegando o resto da divisão de um valor caractere (dividendo)
-            //Por um divisor (99)
-            if (caractere % 99 == 0 )
-                System.out.println(caractere + "\t ");
-            else
-                System.out.print(caractere + "\t ");
+            //Por um divisor (9)
+            if (caractere % 9 == 0) {
+                System.out.println(numero + " [" + caractere + "] ");
+            } else {
+                System.out.print(numero + " [" + caractere + "] ");
+            }
+
             //Somando +1 ao valor atual da variável caractere
             caractere++;
+            numero++;
         }
     }
 }
